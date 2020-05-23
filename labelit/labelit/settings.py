f4faa70a9a95b474c335b23874eeb2dcc9cfee71
Gautham Suresh <gautham.suresh@verloop.io>
2020-05-23 13:59:52 +0530

    Add settings for following : - Projects directory - Configs directory - Exports directory - Temporary storage directory

--- labelit/labelit/settings.py
+++ labelit/labelit/settings.py
@@ -15,6 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+LABELIT_PROJECTS_DIR = os.path.join(BASE_DIR, 'projects')
+LABELIT_CONFIG_DIR = os.path.join(BASE_DIR, 'config')
+LABELIT_EXPORT_DIR = os.path.join(BASE_DIR, 'exports')
+LABELIT_TEMP_DIR = os.path.join(BASE_DIR, 'tmp')
+
+# Create dirs if it doesnt exist
 
 # Setup logging
 LOGGING = {
