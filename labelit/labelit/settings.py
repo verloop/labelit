87ebf24537514b2349dfbee4a910a6ed21882a1e
Gautham Suresh <gautham.suresh@verloop.io>
2020-06-06 17:54:45 +0530

    Add django crispy forms

--- labelit/labelit/settings.py
+++ labelit/labelit/settings.py
@@ -80,7 +80,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
